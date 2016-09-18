8ed2f64c737f051fdc10a85191bf80c489f5607e
Agung Pambudi <agung@qasico.com>
2016-09-19 06:03:34 +0700

    fixing choose layout

--- v2/zvc4254wh5367t2/wbs/theme/capullo/preview/js/custom.js
+++ v2/zvc4254wh5367t2/wbs/theme/capullo/preview/js/custom.js
@@ -4,20 +4,6 @@ $(document).ready(function() {
 
 
 /* =================================
-   LOADER                     
-=================================== */
-// makes sure the whole site is loaded
-$(window).on('load', function() {
-
-    // will first fade out the loading animation
-    $(".loader-inner").fadeOut();
-    // will fade out the whole DIV that covers the website.
-    $(".loader").fadeOut("slow");
-
-});
-
-
-/* =================================
    NAVBAR COLLAPSE ON SCROLL
 =================================== */
 $(window).on('scroll', function(){
@@ -49,79 +35,6 @@ $('a.smooth-scroll').on('click', function(event) {
         scrollTop: $($anchor.attr('href')).offset().top + 20
     }, 1500, 'easeInOutExpo');
     event.preventDefault();
-});
-
-
-//typed.js
-    $(function () {
-
-        $('.typed-element').typed({
-
-            strings: ['COMING SOON <span class="colored">TEMPLATE.</span>', 'NICE AND CLEAN <span class="colored">DESIGN.</span>', 'RESPONSIVE HTML <span class="colored">TEMPLATE.</span>'],
-            // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-            // stringsElement: null,
-            // typing speed
-            typeSpeed: 30,
-            // time before typing starts
-            startDelay: 0,
-            // backspacing speed
-            backSpeed: 0,
-            // shuffle the strings
-            shuffle: false,
-            // time before backspacing
-            backDelay: 2000,
-            // loop
-            loop: true,
-            // false = infinite
-            loopCount: false,
-            // show cursor
-            showCursor: true,
-            // character for cursor
-            cursorChar: " _",
-            // attribute to type (null == text)
-            attr: null,
-            // either html or text
-            contentType: 'html'
-                // call when done callback function
-                // callback: function() {},
-                // starting callback function before each string
-                // preStringTyped: function() {},
-                //callback for every typed string
-                // onStringTyped: function() {},
-                // callback for reset
-                // resetCallback: function() {}
-
-        });
-
-    });
-
-
-/* ===========================================================
-   BACK TO TOP BUTTON
-============================================================== */
-var offset = 300,
-//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-offset_opacity = 1200,
-//duration of the top scrolling animation (in ms)
-scroll_top_duration = 700,
-//grab the "back to top" link
-$back_to_top = $('.top');
-
-//hide or show the "back to top" link
-$(window).on('scroll', function() {
-    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('is-visible') : $back_to_top.removeClass('is-visible fade-out');
-    if( $(this).scrollTop() > offset_opacity ) {
-        $back_to_top.addClass('fade-out');
-    }
-});
-
-//smooth scroll to top
-$back_to_top.on('click', function(event){
-    event.preventDefault();
-    $('body,html').animate({
-        scrollTop: 0
-        }, scroll_top_duration
-    );
 });
 
 
@@ -268,53 +181,6 @@ $('.features-tab .tab-title').on('click', function(e) {
 
 
 /* ===========================================================
-   TWITTER FEED
-============================================================== */
-function handleTweets(tweets) {
-    var x = tweets.length,
-    n = 0,
-    element = document.getElementById('twitter-feed'),
-    html = '<div class="slides">';
-    while (n < x) {
-        html += '<div>' + tweets[n] + '</div>';
-        n++;
-    }
-    html += '</div>';
-    element.innerHTML = html;
-        
-    /* Twits attached to owl-carousel */
-    $("#twitter-feed .slides").owlCarousel({
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        autoPlay: true,
-        pagination: false,
-        transitionStyle : "fade",
-        singleItem: true
-    });
-}
-
-if( $('#twitter-feed').length ) {   
-    var widgetId = $('#twitter-feed').attr('data-widget-id');
-
-    var config_feed = {
-      "id": widgetId,
-      "domId": 'twitter-feed',
-      "maxTweets": 5,
-      "enableLinks": true,
-      "showUser": false,
-      "showTime": true,
-      "dateFunction": '',
-      "showRetweet": false,
-      "customCallback": handleTweets,
-      "showInteraction": false
-    };
-
-    twitterFetcher.fetch(config_feed);
-}
-
-
-
-/* ===========================================================
    COUNTDOWN TIMER
 ============================================================== */
 if( $('.countdown').length ) {
@@ -337,21 +203,6 @@ if( $('#mailchimp-form').length ) {
 }
 
 
-/* ===========================================================
-   GOOGLE MAPS
-============================================================== */
-/* active mouse scroll when the user clicks into the map*/
-if( $('.map-container').length ) {
-    $('.map-container').on('click', function () {
-        $('.map-iframe').css("pointer-events", "auto");
-    });
-
-    $( ".map-container" ).on('mouseleave', function() {
-      $('.map-iframe').css("pointer-events", "none");
-    });
-}
-
-
 /* ==========================================
    FUNCTION FOR EMAIL ADDRESS VALIDATION
 ============================================= */
@@ -359,46 +210,6 @@ function isValidEmail(emailAddress) {
     var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
     return pattern.test(emailAddress);
 }
-
-
-/* ==========================================
-   FUNCTION FOR PHONE NUMBER VALIDATION
-============================================= */
-function isValidPhoneNumber(phoneNumber) {
-    return phoneNumber.match(/[0-9-()+]{3,20}/);
-}
-
-
-/* ==========================================
-   CONTACT FORM
-============================================= */
-$("#contactForm").on('submit', function(e) {
-    
-    e.preventDefault();
-    var data = {
-        name: $("#cfName").val(),
-        email: $("#cfEmail").val(),
-        subject: $("#cfSubject").val(),
-        message: $("#cfMessage").val()
-    };
-
-    if ( isValidEmail(data['email']) && (data['message'].length > 1) && (data['name'].length > 1) && (data['subject'].length > 1) ) {
-        $.ajax({
-            type: "POST",
-            url: "php/contact.php",
-            data: data,
-            success: function() {
-                $('.success.cf').delay(500).fadeIn(1000);
-                $('.failed.cf').fadeOut(500);
-            }
-        });
-    } else {
-        $('.failed.cf').delay(500).fadeIn(1000);
-        $('.success.cf').fadeOut(500);
-    }
-
-    return false;
-});
 
 
 /* ==========================================
@@ -432,81 +243,6 @@ $("#callbackForm").on('submit', function(e) {
 
 
 /* ==========================================
-   TICKET SELECTION
-============================================= */
-var $ticketSelected = $('.ticket-prices .item-price');
-
-$ticketSelected.on('click', function(event) {
-    $ticketSelected.removeClass('active');
-    $(this).addClass('active');
-
-    $('#ticketForm input[name="ticket"]').val($(this).find('h4').text() + ' Ticket - Cost: ' + $(this).find('.amount').text());
-});
-
-
-/* ==========================================
-   TICKET FORM
-============================================= */
-$("#ticketForm").on('submit', function(e) {
-    e.preventDefault();
-    var data = {
-        name: $("#tfName").val(),
-        email: $("#tfEmail").val(),
-        phone: $("#tfPhone").val(),
-        ticket: $("#tfTicket").val()
-    };
-
-    if ( isValidEmail(data['email']) && (data['name'].length > 1) && (data['ticket'].length > 1) && isValidPhoneNumber(data['phone']) ) {
-        $.ajax({
-            type: "POST",
-            url: "php/ticket.php",
-            data: data,
-            success: function() {
-                $('.success.tf').delay(500).fadeIn(1000);
-                $('.failed.tf').fadeOut(500);
-            }
-        });
-    } else {
-        $('.failed.tf').delay(500).fadeIn(1000);
-        $('.success.tf').fadeOut(500);
-    }
-
-    return false;
-});
-
-
-/* ==========================================
-   QUOTE FORM
-============================================= */
-$("#quoteForm").on('submit', function(e) {
-    e.preventDefault();
-    var data = {
-        name: $("#qName").val(),
-        email: $("#qEmail").val(),
-        phone: $("#qPhone").val(),
-        message: $("#qMessage").val()
-    };
-
-    if ( isValidEmail(data['email']) && (data['name'].length > 1) && (data['message'].length > 1) && isValidPhoneNumber(data['phone']) ) {
-        $.ajax({
-            type: "POST",
-            url: "php/quote.php",
-            data: data,
-            success: function() {
-                $('.success.qf').delay(500).fadeIn(1000);
-                $('.failed.qf').fadeOut(500);
-            }
-        });
-    } else {
-        $('.failed.qf').delay(500).fadeIn(1000);
-        $('.success.qf').fadeOut(500);
-    }
-
-    return false;
-});
-
-
-/* ==========================================
    DATEPICKER
 ============================================= */
 if( $("#dfDate").length ) {
@@ -514,38 +250,6 @@ if( $("#dfDate").length ) {
         min: new Date()
     });
 }
-
-
-/* ==========================================
-   APPOINTMENT WITH DATEPICKER FORM
-============================================= */
-$("#dateForm").on('submit', function(e) {
-    e.preventDefault();
-    var data = {
-        name: $("#dfName").val(),
-        email: $("#dfEmail").val(),
-        phone: $("#dfPhone").val(),
-        date: $("#dfDate").val(),
-        message: $("#dfMessage").val()
-    };
-
-    if ( isValidEmail(data['email']) && (data['name'].length > 1) && (data['date'].length > 1) && (data['message'].length > 1) && isValidPhoneNumber(data['phone']) ) {
-        $.ajax({
-            type: "POST",
-            url: "php/appointment.php",
-            data: data,
-            success: function() {
-                $('.success.df').delay(500).fadeIn(1000);
-                $('.failed.df').fadeOut(500);
-            }
-        });
-    } else {
-        $('.failed.df').delay(500).fadeIn(1000);
-        $('.success.df').fadeOut(500);
-    }
-
-    return false;
-});
 
 
 /* ==========================================
@@ -574,36 +278,6 @@ $("#subscribeForm").on('submit', function(e) {
 
     return false;
 });
-
-
-/* ==========================================
-   SUBSCRIBE FORM 2 / EMAIL + NAME
-============================================= */
-$("#subscribeForm2").on('submit', function(e) {
-    e.preventDefault();
-    var data = {
-        name: $("#sf2Name").val(),
-        email: $("#sf2Email").val()
-    };
-        
-    if ( isValidEmail(data['email']) && (data['name'].length > 1) ) {
-        $.ajax({
-            type: "POST",
-            url: "php/subscribe2.php",
-            data: data,
-            success: function() {
-                $('.success.sf2').delay(500).fadeIn(1000);
-                $('.failed.sf2').fadeOut(500);
-            }
-        });
-    } else {
-        $('.failed.sf2').delay(500).fadeIn(1000);
-        $('.success.sf2').fadeOut(500);
-    }
-
-    return false;
-});
-
 
 /* ===========================================================
    BOOTSTRAP FIX FOR IE10 in Windows 8 and Windows Phone 8  
